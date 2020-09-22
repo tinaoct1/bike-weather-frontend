0993ba884fa9f7bfbd9eb87ad47ba8345d0b71bc
coder_0110 <tinaoct1@gmail.com>
2020-09-22 15:21:45 +0400

    extracted fe

---
+++ build/precache-manifest.8d09843967354c46d7e3146007d0d042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd77db54841b7108d146",
+    "url": "/static/js/main.30326bee.chunk.js"
+  },
+  {
+    "revision": "7735fc10b15211b8f3a0",
+    "url": "/static/js/2.119f5023.chunk.js"
+  },
+  {
+    "revision": "dd77db54841b7108d146",
+    "url": "/static/css/main.5d50d596.chunk.css"
+  },
+  {
+    "revision": "246273a85bf038211809cb2e2a0c8155",
+    "url": "/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import moment from 'moment';
+import './styles.css';
+const API_HOST = process.env.APP_ENV === "PRODUCTION" ? "https://bike-weather.herokuapp.com/api/v1":
+	"http://localhost:3000/api/v1"
+function App() {
+	const [stations, setStations] = useState(null);
+	const [at, setAt] = useState(null);
+	const [kioskId, setKioskId] = useState(null);
+	const fetchData = async () => {
+		const url =`${API_HOST}/stations${kioskId ? "/"+kioskId + "?": "?"}at=${at}`
+		const response = await axios.get(
+			url,
+			{
+				headers: {
+					"api-token": "dev-123",
+					'Content-Type': 'application/json',
+				}
+			}
+		);
+		
+		setStations(response.data);
+	};
+	
+	const setTime = (a) => {
+		setAt(a)
+	}
+	const setKiosk = (a) => {
+		setKioskId(a)
+	}
+	
+	
+	
+	return (
+		<div className="App">
+		<h1>Bikes Availability</h1>
+	
+	<div>
+	
+	<div >
+	<label>
+	Time:
+<input className="text-input" onChange={event => setTime(event.target.value)} />
+<br />
+	<br />
+	
+	Kiosk:
+<input className="text-input" onChange={event => setKiosk(event.target.value)} />
+
+</label>
+	</div>
+	
+	<br />
+	
+	<button className="fetch-button" onClick={fetchData}>
+		Fetch Data
+	</button>
+	
+	<br />
+	</div>
+	
+	{/* Display data from API */}
+	<div className="at"> {stations && moment(stations.at).format('LLL')}</div>
+	<div className="weather"> {stations && stations.weather.weather[0].description}</div>
+	<div className="stations">
+		{stations &&
+		stations.stations.map((station, index) => {
+			return (
+		<div className="station" key={index}>
+		<h3>Station {index + 1}</h3>
+	<h2>{station.properties.name}</h2>
+	<div className="details">
+		<p>Bikes Available: {station.properties.bikesAvailable}</p>
+	<p>Address: {station.properties.addressStreet}</p>
+	</div>
+	</div>
+);
+})}
+</div>
+	
+	</div>
+);
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
